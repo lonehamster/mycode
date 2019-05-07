2d2957297e9f65dbd7e2244606ed3e15ada2a67e
lonehamster <46115189+lonehamster@users.noreply.github.com>
2019-05-07 15:19:11 +0800

    Add files via upload

---
+++ ubuntu_xrdp_desktop.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+echo -e "${BLUE}1-升级系统${NC}"
+sudo apt-get update && sudo apt-get dist-upgrade -y
+echo -e "${BLUE}2-安装中文字符集${NC}"
+sudo apt-get install xfonts-intl-chinese && sudo apt-get install ttf-wqy-microhei -y
+echo -e "${BLUE}3-下载git工具${NC}"
+sudo apt-get install git -y
+echo -e "${BLUE}4-安装xrdp${NC}"
+sudo apt-get install tightvncserver xrdp -y
+echo -e "${BLUE}5-安装xubuntu桌面${NC}"
+sudo apt-get install xubuntu-desktop -y
+echo xfce4-session >~/.xsession
+sed -i '/./etc/X11/Xsession/  ixfce4-session' /etc/xrdp/startwm.sh
+sed -i '1 ixfce4-session' /etc/X11/Xsession/
+sudo service xrdp restart
